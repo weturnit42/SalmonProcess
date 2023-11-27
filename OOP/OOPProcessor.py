8afce45b1f01275df2bbca1709601d86c7254be7
박정우 <2018008386@hanyang.ac.kr>
2023-11-27 17:28:13 +0900

    more courses

--- OOP/OOPProcessor.py
+++ OOP/OOPProcessor.py
@@ -5,7 +5,7 @@ from konlpy.tag import Okt
 from nltk.tokenize import sent_tokenize
 import numpy as np
 
-f = open("inYeBonTextv2.txt", "r", encoding='utf-8')
+f = open("OOPText.txt", "r", encoding='utf-8')
 c = f.read()
 f.close()
 
@@ -24,9 +24,9 @@ maxLen = max([len(x)-1 for x in doc] ) # 0번부터 시작하게 하려고
 print(maxLen)
 vocabSize = len(tokenizer.word_index)+1 #1164 + 1 
 print(vocabSize)
-model=load_model("inYeBonv3.hdf5") # 모델 불러오기 
+model=load_model("OOP.hdf5") # 모델 불러오기 
 
-wordList="학점".split()
+wordList="앙".split()
 reverseWordMap = dict(map(reversed,tokenizer.word_index.items()))
 
 x = pad_sequences( [[tokenizer.word_index[w] for w in wordList[:2] ]] , maxlen=maxLen)
@@ -42,15 +42,58 @@ def predictWord(i, n):
     x = pad_sequences([[tokenizer.word_index[w] for w in wordList[:i]]], maxlen = maxLen)
     p = model.predict(x)[0]
     idx = np.flip(np.argsort(p) , 0)
+
+    prob = []
+    sentences = []
     for j in idx[:n]:
         print(" ".join(wordList[:i]), reverseWordMap[j], "(p={:4.2f}%)".format(100*p[j]))
+        prob.append(p[j])
+        sentences.append((" ".join(wordList[:i]) + reverseWordMap[j]))
 
+    # return sentences, [prob]
     return (" ".join(wordList[:i]) + " " + reverseWordMap[j])
 
-temp = predictWord(1,2)
+temp = predictWord(len(wordList),1)
 
 while(True):
     wordList = temp.split()
     if("eos" in wordList):
         break
-    temp = predictWord(len(temp.split()),2)
+    temp = predictWord(len(wordList),1)
+
+# def log(number):
+#   # log에 0이 들어가는 것을 막기 위해 아주 작은 수를 더해줌.
+#   return np.log(number + 1e-10)
+
+# def naive_beam_search_decoder(predictions, k):
+#   # prediction = (seq_len , V)
+#   sequences = [[list(), 1.0]]
+  
+#   for row in predictions:
+#     print(row)
+#     all_candidates = list()
+    
+#     # 1. 각각의 timestep에서 가능한 후보군으로 확장
+#     for i in range(len(sequences)):
+#       seq, score = sequences[i]
+      
+#       # 2. 확장된 후보 스텝에 대해 점수 계산
+#       for j in range(len(row)):
+#         new_seq = seq + [j]
+#         # print(row)
+#         new_score = score * -log(row[j])
+#         candidate = [new_seq, new_score]
+#         all_candidates.append(candidate)
+    
+# 	# 3. 가능도가 높은 k개의 시퀀스만 남김 
+#     ordered = sorted(all_candidates, key=lambda tup:tup[1]) #점수 기준 정렬
+#     sequences = ordered[:k]
+    
+#   return sequences
+
+# import random
+# seq_len = 2
+# V = 5
+# predictions = [[random.random() for _ in range(V)] for _ in range(seq_len)]
+# print(predictions)
+# print(naive_beam_search_decoder(predictWord(1,2)[1], 2))
---
+++ OOP/OOPTrainer.py
@@ -0,0 +1,84 @@
+# text generation 
+# from konlpy.corpus import kolaw # 헌법데이터
+from konlpy.tag import Okt# 형태소 분석기 
+from nltk.tokenize import sent_tokenize
+
+# c = kolaw.open("constitution.txt").read()
+# # c # len(18884)
+
+f = open("OOPText.txt", "r", encoding='utf-8')
+c = f.read()
+f.close()
+# c = "과제가 귀찮지만 이만큼 할거 없는 수업도 없는듯 중간 과제 개꿀입니다 30분안에 할수있어요 매주 보고서 쓰는것도 없어서 좋습니다 한양대 인정 꿀명강의. 출석잘하고 과제 잘 제출하면 됩니다 과제와 출석만 잘 하면 됩니다 영역 채우기 좋은 교양강의"
+okt = Okt()
+doc0=[" ".join( ["".join(w) for w , t in okt.pos(s)] ) for s in sent_tokenize(c) ] 
+   
+# 텍스트 제너레이션
+from keras.preprocessing.text import Tokenizer 
+from keras.preprocessing.sequence import pad_sequences 
+
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(doc0)
+doc = [ tok for tok in tokenizer.texts_to_sequences(doc0) if len(tok) > 1 ]  # 공백문자 제거를 위해 
+
+maxLen = max([len(x)-1 for x in doc] ) # 0번부터 시작하게 하려고 
+print(maxLen)
+vocabSize = len(tokenizer.word_index)+1 #1164 + 1 
+print(vocabSize)
+
+from keras.utils import to_categorical
+
+def genData(doc, maxLen, vocabSize):
+    for sent in doc:
+        inputs=[]
+        targets=[]
+        for i in range(1, len(sent)): 
+            inputs.append(sent[0:i])
+            targets.append(sent[i])
+        y = to_categorical(targets, vocabSize)
+        inputSeq = pad_sequences(inputs, maxlen=maxLen)
+        yield(inputSeq,y)
+#         print("inputs:", inputs)
+#         print("-"*50)
+#         print("targets:", targets)
+#         print("-"*50)
+
+# def cf():
+#     for i in range(3):
+#         yield i*i 
+        
+# obj = cf()
+# for i in obj : 
+#     print(i)
+    
+# for i , (x,y) in enumerate(genData(doc, maxLen, vocabSize)) :
+#     print(i)
+#     print("x", x.shape, "\n", x)
+#     print("y", y.shape, "\n", y)
+
+import numpy as np
+xdata = [] 
+ydata = [] 
+for (x,y) in genData(doc,maxLen, vocabSize):
+    xdata.append(x)
+    ydata.append(y)
+xdata = np.concatenate(xdata)
+ydata = np.concatenate(ydata)
+
+from keras.layers import Input, Embedding, Dense , LSTM ,Dropout
+from keras.models import Sequential
+
+# 모델 작성 
+model = Sequential() 
+model.add(Embedding(vocabSize,100 , input_length = maxLen)) #1165, 100 
+model.add(LSTM(100, return_sequences = False ))
+model.add(Dropout(0.5))
+model.add(Dense(vocabSize, activation="softmax"))
+
+model.compile(loss="categorical_crossentropy", optimizer ="rmsprop", metrics = ["accuracy"])
+
+model.fit(xdata,ydata, epochs = 20, batch_size=800)
+
+model.save("OOP.hdf5") # 모델 저장 
+
+# --------------------------------------------------------------------------------------------------
