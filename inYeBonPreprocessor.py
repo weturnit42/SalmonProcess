b4d0f00aac618fe4fab6e4b9593818b08ca9334c
박정우 <2018008386@hanyang.ac.kr>
2023-11-27 13:46:19 +0900

    first commit

---
+++ inYeBonPreprocessor.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+
+response = open("inYeBonHTML.txt", "r", encoding='utf-8')
+soup = BeautifulSoup(response, "html.parser")
+
+reviewCount = soup.select("body > div > div > div.pane > div > div.articles")[0]
+reviewTotal = ''
+
+for i in list(range(1, len(reviewCount)-1)):
+    print(i)
+    # body > div > div > div.pane > div > div.articles > div:nth-child(1) > div.text
+    text = reviewCount.select('div:nth-child(' + str(i) + ') > div.text')[0].get_text()
+    reviewTotal = reviewTotal + text + ' <eos> '
+
+f = open("inYeBonTextv2.txt", "w", encoding='utf-8')
+f.write(reviewTotal)
+f.close()
---
+++ inYeBonPreprocessor2.py
@@ -0,0 +1,3 @@
+f = open("inYeBonText.txt", "r", encoding='utf-8')
+c = f.read()
+f.close()
