8701e1d1bbd7b30f06c544773339eedd97b346de
박정우 <2018008386@hanyang.ac.kr>
2023-11-29 13:14:32 +0900

    merge

--- InYeBon/inYeBonProcessor.py
+++ InYeBon/inYeBonProcessor.py
@@ -24,9 +24,9 @@ maxLen = max([len(x)-1 for x in doc] ) # 0번부터 시작하게 하려고
 print(maxLen)
 vocabSize = len(tokenizer.word_index)+1 #1164 + 1 
 print(vocabSize)
-model=load_model("inYeBonv3.hdf5") # 모델 불러오기 
+model=load_model("inYeBon_epochs400.hdf5") # 모델 불러오기 
 
-wordList="이 강의".split()
+wordList="강의 를".split()
 reverseWordMap = dict(map(reversed,tokenizer.word_index.items()))
 
 x = pad_sequences( [[tokenizer.word_index[w] for w in wordList[:2] ]] , maxlen=maxLen)
