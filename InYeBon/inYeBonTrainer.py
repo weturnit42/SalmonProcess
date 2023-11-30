6f22d3eb9dfacff42a80f3c34ef6076cca50b606
weturnit42 <we_turn_it42@naver.com>
2023-11-30 18:35:17 +0900

    epochs 300 batch 400

--- InYeBon/inYeBonTrainer.py
+++ InYeBon/inYeBonTrainer.py
@@ -77,8 +77,8 @@ model.add(Dense(vocabSize, activation="softmax"))
 
 model.compile(loss="categorical_crossentropy", optimizer ="rmsprop", metrics = ["accuracy"])
 
-model.fit(xdata,ydata, epochs = 100, batch_size=800)
+model.fit(xdata,ydata, epochs = 300, batch_size=400)
 
-model.save("inYeBon_epochs100.hdf5") # 모델 저장 
+model.save("inYeBon_epochs300_batch400.hdf5") # 모델 저장 
 
 # --------------------------------------------------------------------------------------------------
