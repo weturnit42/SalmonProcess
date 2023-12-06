c857ef8e7bb0b8d32e979caa95ca2fc949a7150f
weturnit42 <43431270+weturnit42@users.noreply.github.com>
2023-12-06 15:50:11 +0900

    Add files via upload

--- KR-SBERT/test.py
+++ KR-SBERT/test.py
@@ -1,7 +1,8 @@
 import csv
-from sentence_transformers import SentenceTransformer, losses
+from sentence_transformers import losses
 from torch.utils.data import DataLoader
 from sentence_transformers import util
+from sentence_transformers import SentenceTransformer, SentencesDataset, InputExample
 import torch
 
 f = open('SCI_dict.tsv','r', encoding='utf-8')
@@ -20,12 +21,31 @@ f.close()
 
 model = SentenceTransformer('snunlp/KR-SBERT-V40K-klueNLI-augSTS') 
 
-train_dataloader = DataLoader(train_data, shuffle=True, batch_size=16)
-train_loss = losses.CosineSimilarityLoss(model)
+# import random
 
-#Tune the model
-# model.fit(train_objectives=[(train_dataloader, train_loss)], epochs=1, warmup_steps=100)
-# torch.save(model.state_dict(), "test" +  ".pt")
+# train_examples = []
+# train_count = 1000
+
+# for k in range(train_count):
+#     if(k % 50 == 0):
+#         print(k)
+#     i = random.randint(0, len(train_data)-1)
+#     j = -1
+#     while(j <0 or j == i):
+#         j = random.randint(0, len(train_data)-1)
+
+#     train_examples.append(InputExample(texts=[train_data[i][random.randint(0, len(train_data[i])-1)], train_data[i][random.randint(0, len(train_data[i])-1)], train_data[j][random.randint(0, len(train_data[j])-1)]]))
+
+# train_dataset = SentencesDataset(train_examples, model)
+# train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=64)
+# train_loss = losses.TripletLoss(model=model)
+
+# # Tune the model
+# model.fit(train_objectives=[(train_dataloader, train_loss)], epochs=16, warmup_steps=100)
+# torch.save(model.state_dict(), "SCI_epochs_16" +  ".pt")
+
+model_state_dict = torch.load("SCI_epochs_16.pt", map_location=torch.device('cpu'))
+model.load_state_dict(model_state_dict)
 
 import numpy as np
 
@@ -36,7 +56,7 @@ for i in list(range(len(mapper))):
     vectors.append(vector)
     print(i, "done")
 
-text = '교수님이 귀엽습니다!'
+text = '생명공학이란 무엇인가를 배울 수 있었습니다.'
 targetVector = model.encode([text])
 results = []
 for i in list(range(len(mapper))):
