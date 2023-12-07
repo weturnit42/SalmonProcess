493aee19960773f365f654f3dabce98fa1189d57
weturnit42 <43431270+weturnit42@users.noreply.github.com>
2023-12-08 01:53:00 +0900

    train test sep

--- KR-SBERT/data_processor.py
+++ KR-SBERT/data_processor.py
@@ -1,55 +1,81 @@
 import csv
 
-f = open('SCI_dict.tsv','r', encoding='utf-8') # *.dict.tsv 파일은 강의 인덱스 - 강의 이름+교수명 매핑 파일
-rdr = csv.reader(f, delimiter='\t')
-mapper = []
-for row in rdr:
-    mapper.append(row[1])
-f.close()
+tags = ['CLA', 'ETC', 'FUT', 'GLO', 'LIT', 'SCI', 'SOC', 'SOF', 'VIR']
 
-f = open('SCI_data.tsv','r', encoding='utf-8') # *.data.tsv 파일은 강의평, 강의 인덱스 데이터 파일
-rdr = csv.reader(f, delimiter='\t')
-train_data = [[] for _ in list(range(len(mapper)))]
-test_data = []
-for row in rdr:
-    train_data[int(row[1])].append(str(row[0]))
-    test_data.append(row)
-f.close()
+for tag in tags:
+    f = open('data/' + tag + '_dict.tsv','r', encoding='utf-8') # *.dict.tsv 파일은 강의 인덱스 - 강의 이름+교수명 매핑 파일
+    rdr = csv.reader(f, delimiter='\t')
+    mapper = []
+    for row in rdr:
+        mapper.append(row[1])
+    f.close()
 
-import random
+    f = open('data/' + tag + '_data.tsv','r', encoding='utf-8') # *.data.tsv 파일은 강의평, 강의 인덱스 데이터 파일
+    rdr = csv.reader(f, delimiter='\t')
+    data = []
+    for row in rdr:
+        data.append(row)
+    f.close()
 
-train_examples = []
-test_examples = []
-train_count = 8000 # hyper_params.
-test_count = 2000
-batch_size = 16
-epochs = 32
+    import random
 
-for k in range(train_count): # training data 생성. (강의A_강의평1, 강의A_강의평2, 강의B_강의평1) 형식의 튜플을 train_example에 저장
-    i = random.randint(0, len(train_data)-1)
-    j = -1
-    while(j <0 or j == i):
-        j = random.randint(0, len(train_data)-1)
+    train_examples = []
+    test_examples = []
+    train_count = 8000 # hyper_params.
+    test_count = 2000
+    batch_size = 16
+    epochs = 32
 
-    train_examples.append([train_data[i][random.randint(0, len(train_data[i])-1)], train_data[i][random.randint(0, len(train_data[i])-1)], train_data[j][random.randint(0, len(train_data[j])-1)]])
+    preTrain = open('train/' + tag + '_preTrain_data_' + str(train_count+test_count) + '.csv','w', encoding='utf-8')
+    preTest = open('test/' + tag + '_predata_' + str(train_count+test_count) + '.csv','w', encoding='utf-8')
+    for k in list(range(len(data))):
+        if(k % 5 != 0):
+            preTrain.write(data[k][0].replace(',','') + ',' + data[k][1] + '\n')
+        else:
+            preTest.write(data[k][0].replace(',','') + ',' + data[k][1] + '\n')
+    preTrain.close()
+    preTest.close()
 
-randomList = []
-k = 0
-while(k < test_count):
-    l = random.randint(0, len(test_data)-1)
+    preTrain = open('train/' + tag + '_preTrain_data_' + str(train_count+test_count) + '.csv','r', encoding='utf-8')
+    preTrainRdr = csv.reader(preTrain)
+    preTest = open('test/' + tag + '_predata_' + str(train_count+test_count) + '.csv','r', encoding='utf-8')
+    preTestRdr = csv.reader(preTest)
 
-    if(l in randomList):
-        continue
-    else:
-        test_examples.append(test_data[l])
-        k = k+1
+    train_data = [[] for _ in list(range(len(mapper)))]
+    test_data = []
+    for row in preTrainRdr:
+        train_data[int(row[1])].append(str(row[0]))
+    for row in preTestRdr:
+        test_data.append(row)
 
-f = open('train_data_' + str(train_count+test_count) + '.csv','w', encoding='utf-8')
-for train_example in train_examples:
-    f.write(train_example[0].replace(',','') + ',' + train_example[1].replace(',','') + ',' + train_example[2].replace(',','') + '\n')
-f.close()
+    preTrain.close()
+    preTest.close()
 
-f = open('test_data_' + str(train_count+test_count) + '.csv','w', encoding='utf-8')
-for test_example in test_examples:
-    f.write(test_example[0].replace(',','') + ',' + test_example[1] + '\n')
-f.close()
+    for k in range(train_count): # training data 생성. (강의A_강의평1, 강의A_강의평2, 강의B_강의평1) 형식의 튜플을 train_example에 저장
+        i = random.randint(0, len(train_data)-1)
+        j = -1
+        while(j <0 or j == i):
+            j = random.randint(0, len(train_data)-1)
+
+        train_examples.append([train_data[i][random.randint(0, len(train_data[i])-1)], train_data[i][random.randint(0, len(train_data[i])-1)], train_data[j][random.randint(0, len(train_data[j])-1)]])
+
+    randomList = []
+    k = 0
+    while(k < test_count):
+        l = random.randint(0, len(data)-1)
+
+        if(l in randomList):
+            continue
+        else:
+            test_examples.append(data[l])
+            k = k+1
+
+    f = open('train/' + tag + '_train_data_' + str(train_count+test_count) + '.csv','w', encoding='utf-8')
+    for train_example in train_examples:
+        f.write(train_example[0].replace(',','') + ',' + train_example[1].replace(',','') + ',' + train_example[2].replace(',','') + '\n')
+    f.close()
+
+    f = open('test/' + tag + '_test_data_' + str(train_count+test_count) + '.csv','w', encoding='utf-8')
+    for test_example in test_examples:
+        f.write(test_example[0].replace(',','') + ',' + test_example[1] + '\n')
+    f.close()
--- KR-SBERT/test.py
+++ KR-SBERT/test.py
@@ -7,7 +7,10 @@ from sentence_transformers import util
 from sentence_transformers import SentenceTransformer, SentencesDataset, InputExample
 import torch
 
-f = open('SCI_dict.tsv','r', encoding='utf-8') # *.dict.tsv 파일은 강의 인덱스 - 강의 이름+교수명 매핑 파일
+tags = ['CLA', 'ETC', 'FUT', 'GLO', 'LIT', 'SCI', 'SOC', 'SOF', 'VIR']
+tag = tags[0]
+
+f = open('data/' + tag + '_dict.tsv','r', encoding='utf-8') # *.dict.tsv 파일은 강의 인덱스 - 강의 이름+교수명 매핑 파일
 rdr = csv.reader(f, delimiter='\t')
 mapper = []
 for row in rdr:
@@ -18,20 +21,20 @@ device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 train_count = 8000 # hyper_params.
 test_count = 2000
 batch_size = 16
-epochs = 64
+epochs = 1
 
 model = SentenceTransformer('snunlp/KR-SBERT-V40K-klueNLI-augSTS', device=device) # pre-trained 모델 불러오기
 model.eval() # gpu 너무 많이 잡아 먹어서 달아놓은 코드
 
 train_examples = []
-f = open('train_data_' + str(train_count+test_count) + '.csv','r', encoding='utf-8')
+f = open('train/' + tag + '_train_data_' + str(train_count+test_count) + '.csv','r', encoding='utf-8')
 rdr = csv.reader(f)
 for row in rdr:
     train_examples.append(InputExample(texts=[row[0], row[1], row[2]]))
 f.close()
 
 test_examples = []
-f = open('test_data_' + str(train_count+test_count) + '.csv','r', encoding='utf-8')
+f = open('test/' + tag + '_test_data_' + str(train_count+test_count) + '.csv','r', encoding='utf-8')
 rdr = csv.reader(f)
 for row in rdr:
     test_examples.append(row)
@@ -46,24 +49,23 @@ train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=batch_size
 train_loss = losses.TripletLoss(model=model) # loss 정의. TripletLoss로
 
 model.fit(train_objectives=[(train_dataloader, train_loss)], epochs=epochs, warmup_steps=100) # fit
+torch.save(model.state_dict(), "fineTunedModel/" + tag + "/" + tag + "_epochs_" + str(epochs) + "_batchsize_" + str(batch_size) +  "_dataset_"  + str(train_count+test_count) +  ".pt") # 모델 저장
 
-torch.save(model.state_dict(), "SCI_epochs_" + str(epochs) + "_batchsize_" + str(batch_size) +  "_dataset_"  + str(train_count+test_count) +  ".pt") # 모델 저장
-
-# model_state_dict = torch.load("SCI_epochs_" + str(epochs) + "_batchsize_" + str(batch_size) +  ".pt", map_location=device)
+# model_state_dict = torch.load("fineTunedModel/" + tag + "_epochs_" + str(epochs) + "_batchsize_" + str(batch_size) +  "_dataset_"  + str(train_count+test_count) +  ".pt", map_location=device)
 # model.load_state_dict(model_state_dict)
 
-f = open('SCI_data.tsv','r', encoding='utf-8') # *.data.tsv 파일은 강의평, 강의 인덱스 데이터 파일
+f = open('data/' + tag + '_data.tsv','r', encoding='utf-8')  # *.data.tsv 파일은 강의평, 강의 인덱스 데이터 파일
 rdr = csv.reader(f, delimiter='\t')
-train_data = [[] for _ in list(range(len(mapper)))]
+test_data = [[] for _ in list(range(len(mapper)))]
 for row in rdr:
-    train_data[int(row[1])].append(str(row[0]))
+    test_data[int(row[1])].append(str(row[0]))
 f.close()
 
 import numpy as np
 
 vectors = [] 
 for i in list(range(len(mapper))): # 각 강의평마다 주어진 강의평 벡터의 평균을 각 강의와 매핑합니다.
-    vector = model.encode(train_data[i])
+    vector = model.encode(test_data[i])
     vector = np.mean(vector, axis=0)
     vectors.append(vector)
     print(i, "done")
@@ -104,7 +106,7 @@ for i in list(range(test_count)):
     if(answer in answerList[:10]): # hits@10 계산
         hitsAt10 = hitsAt10+1
     # rankingBasedMetric 계산. 정답을 n개 중에서 1등으로 예측하면 1점, 2등으로 예측하면 (1-1/n)점, 3등으로 예측하면 (1-2/n)점, ... 이런 식으로 계산
-    rankingBasedMetric = rankingBasedMetric + (1 - (answerList.index(answer)-1) / len(answerList)) 
+    rankingBasedMetric = rankingBasedMetric + (1 - answerList.index(answer) / len(answerList)) 
 
 # 전체 metric은 최댓값이 1입니다.
 acc = acc / test_count
